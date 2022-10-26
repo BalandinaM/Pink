043e669c4e2f5e8207808bf9b6efe235d0dbf0d6
BalandinaM <marina.balandina.89@mail.ru>
2022-10-27 00:48:11 +0500

    Добавила обортку на лого и кнопку в хедере

--- source/js/mobile-menu.js
+++ source/js/mobile-menu.js
@@ -1,17 +1,17 @@
 let navMain = document.querySelector('.header__nav');
 let navToggle = document.querySelector('.header__button');
-let headerLogo = document.querySelector('.header__logo');
+let wrapLogo = document.querySelector('.header__wrap-logo');
 
 navMain.classList.remove ('header__nav--nojs');
 
 navToggle.addEventListener('click', function() {
   if (navMain.classList.contains('header__nav--closed')) {
-    headerLogo.classList.remove('header__logo--bg-opacity');
+    wrapLogo.classList.remove('header__wrap-logo--bg-opacity');
     navMain.classList.remove('header__nav--closed');
     navMain.classList.add('header__nav--open');
   } else {
-    navMain.classList.add('header__nav--closed');
     navMain.classList.remove('header__nav--open');
-    headerLogo.classList.add('header__logo--bg-opacity');
+    navMain.classList.add('header__nav--closed');
+    wrapLogo.classList.add('header__wrap-logo--bg-opacity');
   }
 });
